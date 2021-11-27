9184b7f7da58d3490eff0e492896ed90512fabf9
Scott Payfer <scottpayfer@gmail.com>
2021-11-27 12:25:18 -0800

    created basic_list.py to print a list of names and ages

---
+++ basic_list.py
@@ -0,0 +1,12 @@
+# This script creates a basic list of comma separated values.
+
+nested_list = [["Bob", 43], ["Jimmy", 42], ["Lenny", 68], ["Tillman", 34], ["Fraber", 31]]
+
+
+print("Birth Order, Name, Age");
+print(f"{nested_list[0]}");
+print(f"{nested_list[1]}");
+print(f"{nested_list[2]}");
+print(f"{nested_list[3]}");
+print(f"{nested_list[4]}")
+ 
---
+++ basic_multiplication.py
@@ -0,0 +1,7 @@
+#This performs a basic muliplication calculation based on variable "value".
+
+value = 95.42
+
+print(f"Oracle Stock Price is {value}. Total value of current holdings is: ${value * 10000}")
+
+
